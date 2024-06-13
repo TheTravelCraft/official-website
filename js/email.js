92f2d08fe3df787ce7d07a66233ff06a71e96395
TheTravelCraft <manthanra37@gmail.com>
2024-06-13 22:44:30 +0530

    Add files via upload

---
+++ js/email.js
@@ -0,0 +1,18 @@
+﻿
+function sendemail(){
+
+email = document.getElementById("inputEmail").value;
+message = 
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "askabhay.info@gmail.com",
+    Password : "1D884B3E4A02B33012E4F4B2EEBC39CE74F6",
+    To : 'manthanra36@gmail.com',
+    From : email,
+    Subject : "Inquire",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+
+}
